4242bb07e0e7f9050c2791baa2a07a263f2cc039
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-09-05 14:01:37 +0100

    Packages update

--- Comp-vis.py
+++ Comp-vis.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as nu
 import matplotlib as mat
-import tourch
-import tourchvision
-from tourchvision import transforms
+import tensorflow as tf
+import keras
+from keras import layers, models, optimizers
+from keras.utils import load_img, img_to_array, save_img
+from keras.preprocessing.image import ImageDataGenerator
