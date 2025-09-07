68499a4ad6bddbb9d6961708ed26148199ef561d
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-09-07 15:18:38 +0100

    Web scrape images

--- Comp-vis.py
+++ Comp-vis.py
@@ -6,3 +6,11 @@ import keras
 from keras import layers, models, optimizers
 from keras.utils import load_img, img_to_array, save_img
 from keras.preprocessing.image import ImageDataGenerator
+from bs4 import BeautifulSoup
+import requests
+
+# Web scrape images from popular grocery stores
+def scrape_img():
+    imgs = request.get("https://www.google.com/search?q=groceries&tbm=isch")
+    soup = BeautifulSoup(imgs.text, "html.parser")
+    return(soup)
